6013cab357fcd96a0bf22731d8897c0e43f2c4e0
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-06-22 18:17:05 +0530

    Date and Guest count

--- app/scrapper.rb
+++ app/scrapper.rb
@@ -2,14 +2,41 @@ class Scrapper
 
 	def initialize(site_to_crawl, city)
 		@browser = Watir::Browser.new
+		Selenium::WebDriver.logger.level = :error
 		@site_to_crawl = site_to_crawl
 		@city = city
 	end 
 	
 	def browse_website
-  	@browser.goto @site_to_crawl
+		@browser.goto @site_to_crawl
 		@browser.text_field(id: 'autoComplete__home').set @city
 		@browser.div(class: 'geoSuggestionsList__container').divs.first.click
+		
+		@browser.div(:class => 'geoSuggestionsList__container').wait_until(&:present?)
+		
+		# Open Datepicker
+		@browser.div(:class => "datePickerDesktop__checkInOut").click
+
+		# select month 
+		@browser.select(class: 'DateRangePicker__MonthHeaderSelect').select('9')
+		
+		# select date
+		@browser.spans(class: 'DateRangePicker__DateLabel').each do |element|
+			element.click if element.text == '6'
+			if element.text == '9'
+				element.click
+				break
+			end
+		end
+		
+		# Open Datepicker
+		@browser.div(class: "guestRoomPicker--home").click
+		
+		@browser.span(:class => 'guestRoomPickerPopUp__plus').wait_until(&:present?)
+
+		@browser.span(:class => "guestRoomPickerPopUp__plus").click
+		
+		# Click Search Button
 		@browser.button(class: 'searchButton--home').click
 		@browser.div(:class => 'hotelCardListing__descriptionWrapper').wait_until(&:present?)
 	end
